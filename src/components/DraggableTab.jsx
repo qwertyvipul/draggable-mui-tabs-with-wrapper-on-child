702a75f1fceba2b0d2be30752f522527e10fadfc
Vipul Sharma <vipuls526@gmail.com>
2022-02-16 15:08:53 +0530

    wrapped child

--- src/components/DraggableTab.jsx
+++ src/components/DraggableTab.jsx
@@ -1,20 +1,20 @@
-import * as React from "react";
-import { Draggable } from "react-beautiful-dnd";
-import Tab from "@mui/material/Tab";
+import * as React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
+import Tab from '@mui/material/Tab';
 
 export default function DraggableTab(props) {
-  console.log(props.child);
-  return (
-    <Draggable draggableId={`${props.index}`} index={props.index}>
-      {(draggableProvided) => (
-        <div
-          ref={draggableProvided.innerRef}
-          {...draggableProvided.draggableProps}
-        >
-          <span {...draggableProvided.dragHandleProps}>||</span>
-          {props.child}
-        </div>
-      )}
-    </Draggable>
-  );
+    console.log(props.child);
+    return (
+        <Draggable draggableId={`${props.index}`} index={props.index}>
+            {(draggableProvided) => (
+                <div
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.draggableProps}
+                >
+                    <span {...draggableProvided.dragHandleProps}>||</span>
+                    {props.child}
+                </div>
+            )}
+        </Draggable>
+    );
 }
